da7f31f89839447bbda951ff4c29115f0111d110
Chris Bosse <30784811+chrisBosse@users.noreply.github.com>
2019-05-17 09:26:54 -0400

    Setup Yarn and Node for non-interactive Bash

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
+# Setup Node
+export NVM_DIR="$HOME/.nvm"
 # shellcheck source=/dev/null
-. "$HOME/.profile"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# Setup yarn
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 exec "$@"
